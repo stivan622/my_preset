234258c1f4b13bd505578dddc0e3ee9de3c64035
stivan622 <stivan.tr@gmail.com>
2017-11-21 20:40:14 +0900

    scaffold user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+  User.create!(name: i)
+end
