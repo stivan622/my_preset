6050be9dc0a94ef2f362cda097fc6a6ec4c5493e
stivan622 <stivan.tr@gmail.com>
2017-11-28 18:06:31 +0900

    add page method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,12 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.page(page_id)
+    return all if page_id.nil?
+    default_per_page = 25
+    page_id = page_id.to_i
+    offset_size = default_per_page * (page_id - 1)
+    offset(offset_size).limit(default_per_page)
+  end
 end
